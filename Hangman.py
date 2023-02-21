26555b2a46ccd478de35c3a3a376778cb568372c
Konstantinos Chanioglou <konstantinosch@hotmail.co.uk>
2023-02-21 11:08:24 +0200

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,37 +1,13 @@
-'''
-The hangman game is a word guessing game where the player is given a word and has to guess the letters that make up the word. 
-The player is given a certain number of tries (no more than 6 wrong guesses are allowed) to guess the correct letters before the game is over.
-'''
-
-# Output
-'''
-You have 6 tries left.                                                                                                                                           
-Used letters:                                                                                                                                                    
-Word: _ _ _ _                                                                                                                                                    
-Guess a letter: a 
-
-You have 6 tries left.                                                                                                                                           
-Used letters: a                                                                                                                                                  
-Word: _ a _ a                                                                                                                                                    
-Guess a letter: j    
-
-You have 6 tries left.                                                                                                                                           
-Used letters: j a                                                                                                                                                
-Word: j a _ a                                                                                                                                                    
-Guess a letter: v                                                                                                                                                
-You guessed the word java !
-'''
 import os
-
-#Clear console
-os.system('cls')
-
 #Initialize viriables
 allowedTries = 6
 tries = 0
 guessedLetters = [] 
 guessedLetter = ''
 wordFound = False
+
+#Clear console
+os.system('cls')
 
 try:
     #Dynamic input and validation
